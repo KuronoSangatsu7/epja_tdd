e478f48814522c67140fc74010c7bfbe025eec15
Jaffar Totanji <jacepac264@gmail.com>
2023-04-08 11:20:17 +0300

    passed 2nd wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,3 +1,13 @@
-const calc = () => 0;
+const calc = (someString) => {
+    if (someString.length == 0) {
+        return 0
+    }
+
+    let numbers = someString.split(',')
+
+    const ans = numbers.reduce((sum, curVal) => sum + parseInt(curVal), 0)
+    
+    return ans
+};
 
 export default calc;
