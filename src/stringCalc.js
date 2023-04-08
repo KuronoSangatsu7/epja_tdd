0536ebbc6406a01df1f5dcba7f74a01bb97b7b87
Jaffar Totanji <jacepac264@gmail.com>
2023-04-08 11:46:27 +0300

    passed 5th wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -3,7 +3,21 @@ const calc = (someString) => {
     return 0;
   }
 
-  let numbers = someString.split(/[,\n]/);
+  const regexp = /^\/\/(.{1})/
+  const match = someString.match(regexp)
+
+  let numbers = []
+
+  if (match) {
+    let delim = match[1]
+    const newString = someString.slice(3)
+    numbers = newString.split(`${delim}`)
+    console.log(numbers)
+  }
+
+  else {
+    numbers = someString.split(/[,\n]/)
+  }
 
   const ans = numbers.reduce((sum, curVal) => {
     return sum + parseInt(curVal);
